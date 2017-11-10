66004660567037504dc951b67379cd21d7bc8126
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-10 03:20:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110031858) do
+ActiveRecord::Schema.define(version: 20171110032007) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
